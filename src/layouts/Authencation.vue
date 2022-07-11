ceac6eac0c348b5ad51fe68026290e2e5de08864
QuocLam98 <66244757+QuocLam98@users.noreply.github.com>
2022-07-12 00:29:28 +0700

    s

---
+++ src/layouts/Authencation.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Background from '@/assets/img/backrpg.png'
+import Header from '@/components/Header.vue'
+</script>
+
+<template>
+<Header />
+<div :style="'background-image: url('+Background+')'">
+<slot />
+</div>
+
+</template>
+
