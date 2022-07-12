b881a864532c5003c51cc0aad20f5b77972400a7
QuocLam98 <66244757+QuocLam98@users.noreply.github.com>
2022-07-12 17:55:13 +0700

    thêm điều kiện check login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,6 +2,42 @@
 import Authen from '@/layouts/Authencation.vue'
 import bg from '@/assets/img/backrpg.png'
 import nql from '@/assets/img/nql.png'
+import { ref } from 'vue';
+
+const email = ref('')
+const pass = ref('')
+const emailInput = ref<HTMLInputElement>(null as any)
+const passInput = ref<HTMLInputElement>(null as any)
+const error = ref({
+    email: false,
+    pass: false
+    
+})
+
+const login = () => {
+
+  if (email.value === '' || !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value))
+  {
+    error.value.email = true
+    emailInput.value.focus()
+     return;
+  }
+    else {
+    error.value.email = false
+  }
+
+    if (pass.value === '')
+  {
+    error.value.pass = true;
+    passInput.value.focus()
+     return;
+  }
+  else {
+    error.value.pass = false
+  }
+
+
+}
 
 </script>
 
@@ -27,13 +63,15 @@ import nql from '@/assets/img/nql.png'
             <label class="label">
               <span class="label-text">Email</span>
             </label>
-            <input type="text" placeholder="email" class="input input-bordered" />
+            <input type="text" v-model="email" ref="emailInput" placeholder="email" class="input input-bordered" />
+            <span v-if="error.email" class=" text-red-500">Email sai định dạng</span>
           </div>
           <div class="form-control">
             <label class="label">
               <span class="label-text">Mật khẩu</span>
             </label>
-            <input type="text" placeholder="password" class="input input-bordered" />
+            <input type="text"  v-model="pass" ref="passInput" placeholder="password" class="input input-bordered" />
+            <span v-if="error.pass" class=" text-red-500">Mật khẩu không được để trống</span>
             <label class="label">
                 <RouterLink to="/reg" class="label-text-alt link link-hover">Bạn chưa có tài khoản?</RouterLink>
               <a href="#" class="label-text-alt link link-hover">Bạn quên mật khẩu?</a>
